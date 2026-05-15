6827e34eccb713fa590c7c7fa06ab58449f0cb1c
jonga-llucci <jongallucci@gmail.com>
2026-05-15 12:35:20 +0100

    Update sw.js

--- TSX/sw.js
+++ TSX/sw.js
@@ -2,7 +2,10 @@ const CACHE = 'tkd-v1';
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(['/tkd-academy/TSX']))
+    caches.open(CACHE).then(cache => {
+      // Update this path to match your subfolder name
+      return cache.addAll(['./', 'index.html', 'manifest.json']); 
+    })
   );
   self.skipWaiting();
 });
